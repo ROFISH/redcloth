ecda5e3a2f9c61fe27e042422cbcb1a13685db6b
Ryan Alyea <rofish@rofish.net>
2021-01-02 12:44:34 -0700

    Update README and Version

--- lib/redcloth/version.rb
+++ lib/redcloth/version.rb
@@ -1,8 +1,8 @@
 module RedCloth
   module VERSION
     MAJOR = 0
-    MINOR = 8
-    TINY  = 9
+    MINOR = 9
+    TINY  = 0
     RELEASE_CANDIDATE = 'alpha1'
 
     STRING = [MAJOR, MINOR, TINY, RELEASE_CANDIDATE].join('.')
